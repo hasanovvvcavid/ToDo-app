d26c271dff94e7d077fa90891523843b5b478218
hasanovvvcavid <cavidxh-ap106@code.edu.az>
2026-04-08 23:51:37 +0200

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -26,15 +26,10 @@ const allowedOrigins = [
 ].filter(Boolean);
 
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1 || allowedOrigins.includes('*')) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: true,
   credentials: true
 }));
+
 
 app.use(express.json());
 
